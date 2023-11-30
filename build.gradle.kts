36bf55d27b41eabdbe273f116c499bcb1eff3924
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-11-30 22:07:09 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    val kotlinVersion = "1.9.21"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+}
+
+group = "cloud.drakon"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val ktorVersion = "2.3.6"
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/AgileOctopusLambda.kt
@@ -0,0 +1,80 @@
+package cloud.drakon.agileoctopuslambda
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.plus
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+private val ktorClient = HttpClient {
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+        })
+    }
+}
+
+@Serializable
+private class Results(val results: List<StandardUnitRate>)
+
+@Serializable
+private class StandardUnitRate(
+    @SerialName("value_inc_vat") val valueIncVat: Double,
+    @SerialName("valid_from") val validFrom: Instant,
+    @SerialName("valid_to") val validTo: Instant,
+)
+
+private val timeZone = TimeZone.of("Europe/London")
+private val datePeriod = DatePeriod(days = 1)
+
+suspend fun main() {
+    // TODO: Are we working out the start and end times correctly? https://developer.octopus.energy/docs/api/#agile-octopus
+    val tomorrow = Clock.System.now()
+        .toLocalDateTime(timeZone)
+        .date
+        .plus(datePeriod)
+
+    val startOfTomorrow = tomorrow.atStartOfDayIn(timeZone)
+
+    val endOfTomorrow = tomorrow.plus(datePeriod)
+        .atStartOfDayIn(timeZone)
+
+    val standardUnitRates = ktorClient.get("https://api.octopus.energy/v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-C/standard-unit-rates/") {
+        parameter("period_from", startOfTomorrow)
+        parameter("period_to", endOfTomorrow)
+    }.body<Results>()
+        .results
+        .reversed()
+
+    val hourlyRates = buildMap {
+        for (i in 0..44) {
+            put(
+                "${standardUnitRates[i].validFrom.toLocalDateTime(timeZone).time} - ${standardUnitRates[i + 1].validTo.toLocalDateTime(timeZone).time}",
+                standardUnitRates[i].valueIncVat + standardUnitRates[i + 1].valueIncVat
+            )
+        }
+    }
+
+    var minHourlyRate = hourlyRates.entries
+        .first()
+
+    hourlyRates.entries
+        .drop(1) // The first entry is already the value of `minHourlyRate`
+        .forEach {
+            if (it.value < minHourlyRate.value) minHourlyRate = it
+        }
+
+    // TODO: Send SMS
+    println(minHourlyRate.key)
+}
